d1dc59d43468f50c39c8f9d19c62b82de5d934b8
jingzing <louiswang0898@gmail.com>
2016-03-31 20:42:03 +0800

    spring boot init

--- primary/src/main/java/com/jingzing/abby/Application.java
+++ primary/src/main/java/com/jingzing/abby/Application.java
@@ -1,8 +1,20 @@
 package com.jingzing.abby;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by Louis Wang on 2016/3/31.
  */
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
 }
